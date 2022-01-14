c662540834a34d0c1acc28b5924396aac72fe4ff
lissalad <elissalaymon@gmail.com>
2022-01-14 00:36:47 -0800

    captioned hair

--- components/GalleryImage.js
+++ components/GalleryImage.js
@@ -1,7 +1,7 @@
 function galleryImage({ image, caption }) {
   const imgStyle = "w-full md:w-[300px] rounded-sm ";
   const figCapStyle =
-    "text-5xl absolute opacity-0 group-hover:opacity-100 absolute w-full text-center bg-lime-500/40 backdrop-blur py-8 bottom-0 transition-all duration-500";
+    "text-4xl absolute opacity-0 group-hover:opacity-100 absolute w-full text-center bg-lime-500/40 backdrop-blur py-8 bottom-0 transition-all duration-500";
 
   return (
     <div className="relative group flex justify-center m-2">
--- pages/hair.js
+++ pages/hair.js
@@ -11,21 +11,20 @@ export default function Hair() {
 
       {/* <!-- MAIN --> */}
       <main className="flex flex-wrap justify-center flex-row items-center w-screen relative z-[50] md:mt-3 mb-6">
-        <GalleryImage image="/images/hair/hair12.png" caption="amazing hair!" />
-        <GalleryImage image="/images/hair/hair5.jpg" caption="amazing hair!" />
-        <GalleryImage image="/images/hair/hair13.jpg" caption="amazing hair!" />
-        <GalleryImage image="/images/hair/hair14.jpg" caption="amazing hair!" />
-        <GalleryImage image="/images/hair/hair15.jpg" caption="amazing hair!" />
-        <GalleryImage image="/images/hair/hair1.jpg" caption="amazing hair!" />
-        <GalleryImage image="/images/hair/hair2.jpg" caption="amazing hair!" />
-        <GalleryImage image="/images/hair/hair3.jpg" caption="amazing hair!" />
-        <GalleryImage image="/images/hair/hair7.jpg" caption="amazing hair!" />
-        <GalleryImage image="/images/hair/hair10.jpg" caption="amazing hair!" />
-        <GalleryImage image="/images/hair/hair11.jpg" caption="amazing hair!" />
-        <GalleryImage image="/images/hair/hair4.jpg" caption="amazing hair!" />
-        <GalleryImage image="/images/hair/hair6.jpg" caption="amazing hair!" />
-        <GalleryImage image="/images/hair/hair8.jpg" caption="amazing hair!" />
-        <GalleryImage image="/images/hair/hair9.jpg" caption="amazing hair!" />
+        <GalleryImage image="/images/hair/hair12.png" caption="Corls" />
+        <GalleryImage image="/images/hair/hair5.jpg" caption="Snip Snap" />
+        <GalleryImage image="/images/hair/hair13.jpg" caption="Corly Corly" />
+        <GalleryImage image="/images/hair/hair14.jpg" caption="Get Blown" />
+        <GalleryImage image="/images/hair/hair15.jpg" caption="Orange" />
+        <GalleryImage image="/images/hair/hair1.jpg" caption="Hehe He Holds Da Brush" />
+        <GalleryImage image="/images/hair/hair2.jpg" caption="Rope Bwaid!" />
+        <GalleryImage image="/images/hair/hair3.jpg" caption="Mmm Smooth" />
+        <GalleryImage image="/images/hair/hair7.jpg" caption="Mommy" />
+        <GalleryImage image="/images/hair/hair10.jpg" caption="Evil Vixen Woman" />
+        <GalleryImage image="/images/hair/hair4.jpg" caption="Twisty" />
+        <GalleryImage image="/images/hair/hair6.jpg" caption="Ooo Firey" />
+        <GalleryImage image="/images/hair/hair8.jpg" caption="Beach Blonde" />
+        <GalleryImage image="/images/hair/hair9.jpg" caption="Wind Blown Swept" />
       </main>
     </>
   );
